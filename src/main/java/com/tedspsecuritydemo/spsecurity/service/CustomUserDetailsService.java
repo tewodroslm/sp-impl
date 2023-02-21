aba218c0d48ee13681ee9dcf04001703a79c000c
Mamushe <tewodroslemma64@gmail.com>
2023-02-21 06:45:30 -0500

    logger added

--- src/main/java/com/tedspsecuritydemo/spsecurity/service/CustomUserDetailsService.java
+++ src/main/java/com/tedspsecuritydemo/spsecurity/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.tedspsecuritydemo.spsecurity.service;
 import com.tedspsecuritydemo.spsecurity.model.CustomUserDetails;
 import com.tedspsecuritydemo.spsecurity.model.Users;
 import com.tedspsecuritydemo.spsecurity.repository.UsersRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,7 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Slf4j
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -18,8 +19,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     private UsersRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Users> optionalUsers = userRepository.findByName(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("Custom User Details Service ..");
+        Optional<Users> optionalUsers = userRepository.findByEmail(email);
         optionalUsers
                 .orElseThrow(() -> new UsernameNotFoundException("User name not found"));
         return optionalUsers.map(CustomUserDetails::new).get();
