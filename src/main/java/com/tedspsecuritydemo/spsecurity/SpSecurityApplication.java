cb9d49e70574b5841c49a5fbb8047516df34a0ed
Mamushe <tewodroslemma64@gmail.com>
2023-03-01 13:30:36 -0500

    cors configuration

--- src/main/java/com/tedspsecuritydemo/spsecurity/SpSecurityApplication.java
+++ src/main/java/com/tedspsecuritydemo/spsecurity/SpSecurityApplication.java
@@ -2,12 +2,26 @@ package com.tedspsecuritydemo.spsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpSecurityApplication {
 
 	public static void main(String[] args) {
+//		System.setProperty("spring.config.name", "application");
 		SpringApplication.run(SpSecurityApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
