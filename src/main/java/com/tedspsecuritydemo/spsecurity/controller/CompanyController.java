f7237af54ed14b4e59a345babdbe44b2b0d1e6ec
Mamushe <tewodroslemma64@gmail.com>
2023-03-22 13:15:05 -0400

    create company

--- src/main/java/com/tedspsecuritydemo/spsecurity/controller/CompanyController.java
+++ src/main/java/com/tedspsecuritydemo/spsecurity/controller/CompanyController.java
@@ -1,6 +1,6 @@
 package com.tedspsecuritydemo.spsecurity.controller;
 
-import com.tedspsecuritydemo.spsecurity.dto.CompanyResponseDto;
+import com.tedspsecuritydemo.spsecurity.dto.CompanyDto;
 import com.tedspsecuritydemo.spsecurity.service.CompanyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,28 +8,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+
 @Slf4j
-@RestController("/company")
+@RestController
+@RequestMapping("company")
 public class CompanyController {
 
     @Autowired
     CompanyService companyService;
 
     // create new
+//    @RolesAllowed("ADMIN")
     @PostMapping("/create-company")
-    public ResponseEntity<?> creatCompany(@RequestBody String companyName){
+    public ResponseEntity<CompanyDto> createCompany(@RequestBody CompanyDto company){
         log.info("Inside Company Controller");
         // create service
-        CompanyResponseDto companyResponseDto = null;
-        if(companyName != null){
-            companyResponseDto = companyService.create(companyName);
+        CompanyDto companyResponseDto = null;
+        if(company != null){
+            companyResponseDto = companyService.create(company);
             if(companyResponseDto == null){
-                return new ResponseEntity<>("Error creating company", HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<CompanyDto>((CompanyDto) null, HttpStatus.BAD_REQUEST);
             }
         }
-        return new ResponseEntity<>("Created Successfully \n" + companyResponseDto.toString() ,HttpStatus.OK);
+        return new ResponseEntity<>(companyResponseDto ,HttpStatus.OK);
     }
 
     // get All
