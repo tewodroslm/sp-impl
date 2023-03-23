80c524b852cbf92eec6df3c7cace9fb8adaa6776
Mamushe <tewodroslemma64@gmail.com>
2023-03-23 19:39:34 -0400

    status constant collection

---
+++ src/main/java/com/tedspsecuritydemo/spsecurity/config/StatusEnum.java
@@ -0,0 +1,10 @@
+package com.tedspsecuritydemo.spsecurity.config;
+
+public enum StatusEnum {
+    INITIATED, ON_HOLD, CANCELED, APPROVED, PENDING_APPROVAL
+}
+// INITIATED - 1
+// ON_HOLD - 2
+// CANCELED - 3
+// APPROVED - 4
+// PENDING_APPROVAL - 5
